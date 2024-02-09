b951e42c8c909f6f01c498816c057ad96ba32cd6
Mohamed Nizar <nizarp666@gmail.com>
2024-02-09 10:22:14 +0530

    new updation

--- client/src/slices/apiSlice.ts
+++ client/src/slices/apiSlice.ts
@@ -1,6 +1,6 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-const baseQuery=fetchBaseQuery({baseUrl:'http://localhost:9009'})
+const baseQuery=fetchBaseQuery({baseUrl:'https://linguablend.live'})
 
 export const apiSlice=createApi({
     baseQuery,
